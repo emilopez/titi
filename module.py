07ce928e65bda52db964a18c5997f8fe5eaec2e8
Emiliano López <emiliano.lopez@gmail.com>
2014-06-20 01:54:38 -0300

    a new function GetMasiveValues to extract values of multiple images

--- module.py
+++ module.py
@@ -1,3 +1,5 @@
+import rasterIO
+
 def getRowCol(lat,lon,lat0,lon0,dlat,dlon):
     '''Devuelve la fila y columna correspondiente a una lat y long, donde:
         lat,lon: es latitud y longitud del punto a extraer
@@ -16,3 +18,18 @@ def getLatLon(row,col,lat0,lon0,dlat,dlon):
     lat = row*dlat + lat0
     lon = col*dlon + lon0
     return lat,lon
+
+def GetMasiveValues(fileLst, pos, band=1):
+    for fn in fileLst:
+        file = rasterIO.opengdalraster(fn)
+        driver, Nx, Ny, NBand, proj, geo = rasterIO.readrastermeta(file)
+        lon0,lat0,dlon,dlat = geo[0],geo[3],geo[1],geo[5]
+        etrMap = rasterIO.readrasterband(file, band)
+        # Row y Col correspondiente a lat lon
+        print "+ "+fn
+        for po in pos.keys():
+            row,col = getRowCol(pos[po][0],pos[po][1],lat0,lon0,dlat,dlon)
+            if etrMap.mask[row][col]:
+                print "- "+po+"\t"+"Valor Invalido"
+            else:
+                print "- "+po+"\t",etrMap[row][col]
