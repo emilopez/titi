288e6eee095a2155620f20c19b91ae18e7201a37
Emiliano López <emiliano.lopez@gmail.com>
2014-06-25 09:56:24 -0300

    function SaveMasiveValues store on a csv file the results

--- module.py
+++ module.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import rasterIO
+import csv
 
 def getRowCol(lat,lon,lat0,lon0,dlat,dlon):
     '''Devuelve la fila y columna correspondiente a una lat y long, donde:
@@ -24,25 +25,37 @@ def getLatLon(row,col,lat0,lon0,dlat,dlon):
     lon = col*dlon + lon0
     return lat,lon
 
-def GetMasiveValues(fileLst, pos, band=1):
+def SaveMasiveValues(outfilename,fileLst, pos, band=1):
     '''
     Recibe:
-        fileLst: lista de archivos a procesar
-        pos: dict de puntos de observación del tipo {"Santo Domingo": [-31.117368, -60.883001],...}
-        band: banda donde extraer los valores
-    Retorna:
-        <todavia no se>
+        :outfilename: nombre de archivo csv de salida
+        :fileLst: lista de archivos a procesar
+        :pos: dict de puntos de observación del tipo {"Santo Domingo": [-31.117368, -60.883001],...}
+        :band: banda donde extraer los valores
+
     '''
-    for fn in fileLst:
-        file = rasterIO.opengdalraster(fn)
-        driver, Nx, Ny, NBand, proj, geo = rasterIO.readrastermeta(file)
-        lon0,lat0,dlon,dlat = geo[0],geo[3],geo[1],geo[5]
-        etrMap = rasterIO.readrasterband(file, band)
-        # Row y Col correspondiente a lat lon
-        print "+ "+fn
-        for po in pos.keys():
-            row,col = getRowCol(pos[po][0],pos[po][1],lat0,lon0,dlat,dlon)
-            if etrMap.mask[row][col]:
-                print "- "+po+"\t"+"Valor Invalido"
-            else:
-                print "- "+po+"\t",etrMap[row][col]
+    header1 = ['filename'] + pos.keys()
+    header2 = [''] + [str(pos[k][0])+','+str(pos[k][1]) for k in pos.keys()]
+
+    with open(outfilename, 'wb') as csvfile:
+        spamwriter = csv.writer(csvfile, dialect='excel', delimiter=';',
+            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(header1)
+        spamwriter.writerow(header2)
+
+        for fn in fileLst:
+            file = rasterIO.opengdalraster(fn)
+            driver, Nx, Ny, NBand, proj, geo = rasterIO.readrastermeta(file)
+            lon0,lat0,dlon,dlat = geo[0],geo[3],geo[1],geo[5]
+            etrMap = rasterIO.readrasterband(file, band)
+            csvrow = [fn]
+            # Row y Col correspondiente a lat lon
+            #print "+ "+fn
+            for po in pos.keys():
+                row,col = getRowCol(pos[po][0],pos[po][1],lat0,lon0,dlat,dlon)
+                if etrMap.mask[row][col]:
+                    print "- "+po+"\t"+"Valor Invalido"
+                else:
+                    csvrow += [etrMap[row][col]]
+            #print csvrow
+            spamwriter.writerow(csvrow)
