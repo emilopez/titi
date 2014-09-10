a926ac4e576b0531257b0248f7c09bfdbeea40f0
ceneha <elopez@fich.unl.edu.ar>
2014-09-10 12:34:52 -0300

    metadata

--- titiQt/titi.py
+++ titiQt/titi.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 # -*- encoding: utf8 -*-
+
+#===============================================================================
+# META
+#===============================================================================
+
+__version__ = "0.2.0"
+__license__ = "GPL v3"
+__author__ = "Emiliano López, Gabriel García"
+__email__ = "emiliano dot lopezat gmail dot com, gabiagus gmail dot com"
+__url__ = "https://github.com/emilopez/titi"
+__date__ = "2014-09-10"
+
 import sys
 import titi_app
 
