d06c388a1c9ff729cba04ba9f7893267a817fb6f
ceneha <elopez@fich.unl.edu.ar>
2014-09-16 00:59:26 -0300

    typo fixed

--- titiQt/titi.py
+++ titiQt/titi.py
@@ -8,7 +8,7 @@
 __version__ = "0.2.0"
 __license__ = "GPL v3"
 __author__ = "Emiliano López, Gabriel García"
-__email__ = "emiliano dot lopezat gmail dot com, gabiagus gmail dot com"
+__email__ = "emiliano dot lopez at gmail dot com, gabiagus gmail dot com"
 __url__ = "https://github.com/emilopez/titi"
 __date__ = "2014-09-10"
 
